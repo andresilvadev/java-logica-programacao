faee341068ce811bc1b623185f5a16719cde4f4b
Andre Luiz da Silva <andreluiz1013@gmail.com>
2017-05-28 13:19:29 -0300

    Calculando o IMC

---
+++ src/IMC.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IMC {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite o seu peso (em Kg): ");
+		Double peso = scanner.nextDouble();
+				
+		System.out.print("Digite sua altura (em metros): ");
+		Double altura = scanner.nextDouble();			
+		
+		Double imc = peso / (altura * altura); 		
+		
+		System.out.println("Seu IMC é de: " + imc);
+		
+		scanner.close();
+	}
+}
