4180778a01e1b96671e59ac370a2c3ca8787120b
Andre Luiz da Silva <andreluiz1013@gmail.com>
2017-06-11 12:18:15 -0300

    Iteracao numeros divisiveis por 3 com while

---
+++ src/CalculandoNumerosDivisiveisWhile.java
@@ -0,0 +1,24 @@
+
+public class CalculandoNumerosDivisiveisWhile {
+
+	public static void main(String[] args) {
+	
+		Integer[] numeros = new Integer[]{
+			1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20	
+		};
+		
+		
+		int i = 0;		
+		
+		while (i < numeros.length) {
+			Integer numeroDivisivel = numeros[i] % 3;		
+			
+			if(numeroDivisivel.equals(0)){
+				System.out.println("Os numeros: " + numeros[i] + " são divisiveis por 3");				
+			}
+			
+			i ++;
+		}
+	}
+
+}
