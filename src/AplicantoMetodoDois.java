9c76beb25564a42e0ddfda0247d52b9ef39cee83
Andre Luiz da Silva <andreluiz1013@gmail.com>
2017-06-12 00:04:43 -0300

    Ainda trabalhando com metodos

---
+++ src/AplicantoMetodoDois.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class AplicantoMetodoDois {
+
+public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		imprimirTraco();
+		
+		
+		String[] cursos = new String[]{"Java EE", "Spring", "Java OO Avançado"};		
+		System.out.println("Escolha dentre os cursos abaixo: ");		
+			
+		
+		for(int i=0; i < cursos.length; i++){
+			System.out.println("[" + i + "]" +cursos[i]);			
+		}
+		
+		System.out.print("O curso que você deseja é o: ");
+		Integer posicaoCursoEscolhido =  scanner.nextInt();
+		
+		
+		Boolean posicaoValida = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
+		
+		if(!posicaoValida){
+			posicaoInvalida();
+		}
+		
+		
+		imprimirTraco();
+		
+		
+		String[] formasPagamento = new String[]{"Cartão", "Boleto"};
+		
+		System.out.println("Escolha dentre as formas de pagamento abaixo: ");
+		
+		for(int i =0; i < formasPagamento.length; i++){
+			System.out.println("[" + i + "]" + formasPagamento[i]);
+		}
+		
+		
+		System.out.println("Sua forma de pagamento escolhida é: ");
+		Integer posicaoFormaPagamentoEscolhida = scanner.nextInt();
+		
+		posicaoValida = posicaoFormaPagamentoEscolhida >= 0 && posicaoFormaPagamentoEscolhida < formasPagamento.length;
+		
+		if(!posicaoValida){
+			posicaoInvalida();
+		}
+		
+		String cursoEscolhido = cursos[posicaoCursoEscolhido];
+		String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+		
+		imprimirTraco();
+		
+		System.out.println("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhida);
+		
+		scanner.close();
+
+	}
+
+	private static void imprimirTraco() {
+		System.out.println("----------------------------------------------");		
+	}
+	
+	private static void posicaoInvalida(){
+		System.err.println("Posição inválida!");
+		System.exit(1);
+	}
+	
+}
